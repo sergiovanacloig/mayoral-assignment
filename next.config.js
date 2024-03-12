ec0f4077a60aac23823d3951d2361cdc71c74263
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2024-03-12 14:19:44 +0100

    Add mayoral to assets

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@
  */
 const config = {
   reactStrictMode: true,
+  images: {
+    domains: ['assets.mayoral.com'],
+  }
 };
 
 module.exports = config;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import '../styles/global.css';
+import '../styles/app.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main>
       <Head>
         <title>Mayoral assignment</title>
         <link rel="icon" ref="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </main>
   );
 }
