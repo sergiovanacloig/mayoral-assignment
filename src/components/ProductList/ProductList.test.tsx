258bbffd881b13e9b4fa7648d84449ae81c82327
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2024-03-13 20:18:45 +0100

    Use mockProducts on product list test

--- src/components/ProductList/ProductList.test.tsx
+++ src/components/ProductList/ProductList.test.tsx
@@ -1,38 +1,6 @@
-import { Product } from 'utils/schemas/product';
-import { ProductList } from './ProductList';
-
 import { render, screen } from '@testing-library/react';
-
-const mockProducts: Product[] = [
-  {
-    id: '1',
-    name: 'Jersey básico Better Cotton',
-    image: '/path/to/image.jpg',
-    price: 18.99,
-  },
-  {
-    id: '2',
-    name: 'Pantalón deportivo felpa',
-    image: '/path/to/image.jpg',
-    price: 18.99,
-    hasColours: true,
-  },
-  {
-    id: '3',
-    name: 'Camiseta texto Better Cotton',
-    image: '/path/to/image.jpg',
-    price: 18.99,
-    discount: 20,
-    hasColours: true,
-  },
-  {
-    id: '4',
-    name: 'Bermuda sarga Better Cotton',
-    image: '/path/to/image.jpg',
-    price: 18.99,
-    discount: 15,
-  },
-];
+import { ProductList } from './ProductList';
+import { mockProducts } from '../../utils/mocks/products';
 
 describe('ProductList', () => {
   it('should render a list of products', () => {
